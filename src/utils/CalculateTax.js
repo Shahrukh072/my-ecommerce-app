f57b52fe66695cc474917361222e307dc3c3a391
shahrukh khan <shahrukh.khan072@gmail.com>
2023-02-12 21:48:55 +0530

    react based ecommerce app

---
+++ src/utils/CalculateTax.js
@@ -0,0 +1,8 @@
+
+const CalculateTax = subtotal => {
+  return {
+    taxes: parseFloat((subtotal * 0.14).toFixed(2)),
+    total: parseFloat((subtotal * 1.14).toFixed(2))
+  };
+};
+export default CalculateTax;
